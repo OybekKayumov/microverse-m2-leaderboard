0f6987e00bfb4ddaa9a862bf3b68adc39c417ea5
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-05 14:50:25 +0500

    add fetch async await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-// eslint-disable-next-line no-unused-vars
-import _ from 'lodash';
 import './style.css';
+
+import { getScores, setScore } from './scores';
+import renderScore from './render-score';
+
+const refBtn = document.querySelector('.btn-refresh');
+const form = document.querySelector('form');
+
+refBtn.addEventListener('click', async () => {
+  const list = await getScores();
+  renderScore(list);
+});
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const score = {
+    user: form.elements.username.value,
+    score: form.elements.score.value,
+  };
+
+  form.reset();
+  await setScore(score);
+});

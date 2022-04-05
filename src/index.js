9c6bbd9ed246ae6a8852e01fd52842cca0c9f043
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-05 08:28:22 +0500

    add index.html and empty index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  // eslint-disable-next-line no-undef
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
